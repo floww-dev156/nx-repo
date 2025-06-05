940546a6ec46b28ee50ff51324d9c5c95a655af0
DW-MAC-MINI-004 <dw-mac-mini-004@DW-MAC-MINI-004s-Mac-mini.local>
2025-06-05 14:20:03 +0530

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/FivkQ7kgNZ

---
+++ apps/nx-repo-app/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@nx-repo/nx-repo-app" />
+    </div>
+  );
+}
+
+export default App;
